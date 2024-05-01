43ce6c413042bf8a1dc325b39e7dcaa2e1573143
YoussefDarwishh <youssef.darwish03@eng-st.cu.edu.eg>
2024-05-01 19:09:59 +0300

    inital implementation

--- BranchingDecodeUnit.vhd
+++ BranchingDecodeUnit.vhd
@@ -1,1 +1,30 @@
+library ieee;
+use ieee.std_logic_1164.all;
 
+
+entity BranchingDecodeUnit is
+    port (
+        Clk : in std_logic;
+        reset : in std_logic;
+        DisableBranching : in std_logic;
+        Branching : in std_logic;
+        BranchingAddressIn : in std_logic_vector(31 downto 0);
+        FlushDecode : out std_logic;
+        BranchingAddressOut : out std_logic_vector(31 downto 0)
+    );
+end BranchingDecodeUnit;
+
+architecture Behavioral of BranchingDecodeUnit is
+begin
+    process(Clk)
+    begin
+        if reset = '1' then
+            FlushDecode <= '0';
+            BranchingAddressOut <= (others => '0');
+        elsif rising_edge(Clk) then
+            if Branching = '1' then
+                BranchingAddressOut <= BranchingAddressIn;
+            end if;
+        end if;
+    end process;
+end Behavioral;
--- BranchingExecuteUnit.vhd
+++ BranchingExecuteUnit.vhd
@@ -1,1 +1,48 @@
+library ieee;
+use ieee.std_logic_1164.all;
 
+entity BranchingExecuteUnit is 
+    port (
+        Clk : in std_logic;
+        reset : in std_logic;
+        WasPredictionDisabled : in std_logic;
+        PCPlus1 : in std_logic_vector(31 downto 0);
+        ConditionalJumpAddress : in std_logic_vector(31 downto 0);
+        BranchPrediction : in std_logic;
+        ZeroFlag : in std_logic;
+        UnconditionalJump : in std_logic;
+        FlushDecode : out std_logic;
+        FlushExecute : out std_logic;
+        JumpAddress : out std_logic_vector(31 downto 0)
+    );
+end BranchingExecuteUnit;
+
+architecture Behavioral of BranchingExecuteUnit is
+begin
+    process(Clk, reset)
+    begin
+        if reset = '1' then
+            FlushDecode <= '1';
+            FlushExecute <= '1';
+            JumpAddress <= (others => '0');
+        elsif rising_edge(Clk) then
+            if UnconditionalJump = '1' then
+                FlushDecode <= '1';
+                FlushExecute <= '1';
+                JumpAddress <= ConditionalJumpAddress;
+            elsif BranchPrediction = '1' and ZeroFlag = '1' then
+                FlushDecode <= '1';
+                FlushExecute <= '1';
+                JumpAddress <= ConditionalJumpAddress;
+            elsif BranchPrediction = '0' and ZeroFlag = '0' then
+                FlushDecode <= '1';
+                FlushExecute <= '1';
+                JumpAddress <= ConditionalJumpAddress;
+            else
+                FlushDecode <= '0';
+                FlushExecute <= '0';
+                JumpAddress <= PCPlus1;
+            end if;
+        end if;
+    end process;
+end Behavioral;
